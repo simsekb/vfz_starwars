68404958846e83bf45318676e4a8297fe676752a
bsimsek <games_bg@dutchgamestudio.nl>
2021-04-17 22:56:33 +0200

    added models & resource controllers

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,3 +15,12 @@ mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
+
+module.exports = {
+    // ...
+    resolve: {
+        alias: {
+        'vue$': 'vue/dist/vue.esm.js' // 'vue/dist/vue.common.js' for webpack 1
+        }
+    }
+}
